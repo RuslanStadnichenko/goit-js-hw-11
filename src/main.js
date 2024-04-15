2d7d0a2d536779677e20c0fc430e15c3e2b75d4d
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-04-16 01:15:41 +0300

    css fix

--- src/main.js
+++ src/main.js
@@ -1,30 +1,57 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast"; 
+import "izitoast/dist/css/iziToast.min.css";  
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css"; 
+import { requestServer } from './js/pixabay-api.js'; 
+import { createGallery } from './js/render-functions.js'; 
 
-import { requestServer } from './js/pixabay-api.js';
-import { createGallery } from './js/render-functions.js';
-
-const searchForm = document.querySelector('.js-search-form');
-const searchInput = document.querySelector('.js-search-input');
-const listGallery = document.querySelector('.gallery-list');
+const searchForm = document.querySelector('.js-search-form'); 
+const searchInput = document.querySelector('.js-search-input'); 
+const btnSearch = document.querySelector('.search-btn'); 
+const listGallery = document.querySelector('.gallery-list'); 
 const loader = document.querySelector('.loader');
 
+searchForm.addEventListener('submit', (event) => { 
+    event.preventDefault(); 
+    if (searchInput.value.trim() === '') { 
+        iziToast.error({ 
+           message: 'Enter a search name', 
+           position: 'topRight', 
+        }); 
+        listGallery.innerHTML = ''; 
+        return; 
+    } 
 
-searchForm.addEventListener('submit', (event) => {
+    loader.classList.add('is-visible'); // Show loader when search is loading
 
-  event.preventDefault();
+    requestServer(searchInput.value)
+        .then(obj => { 
+            if (!obj.hits.length) { 
+                iziToast.error({ 
+                    title: 'Error', 
+                    message: 'Photo not found', 
+                    position: 'topRight', 
+                }); 
+                return; 
+            } 
+            listGallery.innerHTML = createGallery(obj.hits); 
+            libraryLightBox.refresh(); 
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+            loader.classList.remove('is-visible'); // Hide loader when search is done loading
+        });
 
+    searchInput.value = '';
+});
 
-  if (searchInput.value.trim() === '') {
-    event.preventDefault();
-    iziToast.error({
-      message: 'Enter a search name',
-      position: 'topRight',
-    });
-    listGallery.innerHTML = '';
-    return;
-  }
-})
+let libraryLightBox = new SimpleLightbox('.js-gallery-link', { 
+    captionDelay: 250, 
+    captionsData: 'alt', 
+});
+
+libraryLightBox.on('show.simplelightbox', event => { 
+    event.preventDefault(); 
+});
+
